8d2c13421725fd67e7e940fa1b1e321d1f0225cb
gabeart10 <millergabe10@gmail.com>
2016-12-10 22:00:36 +0000

    save

--- espeak_py.py
+++ espeak_py.py
@@ -8,3 +8,6 @@ def say(text=None,voice=None):
   else:
     voice = ""
   sp.call("espeak" + voice + text, shell=True)
+
+if __name__ in "__main__":
+     sp.call("espeak -v kill -f /home/pi/lyric", shell=True)
